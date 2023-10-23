1cae2b1be01278e183669a3a4f53553fadb6150d
Epicsociety <measkswho@gmail.com>
2023-10-23 15:34:57 +0300

    Return the start and end index of a page

---
+++ 0x00-pagination/0-simple_helper_function.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" Provides the start and end index """
+
+
+def index_range(page: int, page_size: int) -> tuple:
+    """
+    Return the start and end index of a page
+    parameters:
+    page:
+    page_size:
+    """
+    start_index = (page - 1) * page_size
+    end_index = start_index + page_size
+    return (start_index, end_index)
---
+++ 0x00-pagination/README.md
@@ -0,0 +1,4 @@
+##Resources
+
+[REST API Design: Pagination](https://intranet.alxswe.com/rltoken/7Kdzi9CH1LdSfNQ4RaJUQw)
+[HATEOAS](https://intranet.alxswe.com/rltoken/tfzcEbTSdMYSYxsspJH_oA)
