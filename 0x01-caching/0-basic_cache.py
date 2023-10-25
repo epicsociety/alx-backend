353fad766752cf282b744963a3edd3a9c881a0e9
Epicsociety <measkswho@gmail.com>
2023-10-25 20:24:53 +0300

    adds and retrieves an item from Cache

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -9,22 +9,27 @@ BaseCaching = __import__('base_caching').BaseCaching
 
 class BasicCache(BaseCaching):
     """
-    Initialize the class
+    Class inherist from BaseCaching
+    Adds and retrieves an item from Cache
     """
     def __init__(self):
-        """ Initialize the child class
+        """ Initializes the child class
         """
         super().__init__()
 
     def put(self, key, item):
-        """ Add an item in the cache
+        """ Add an item to the self.cache_data
+        Parameters:
+        Key:
+        Item:
         """
         if key is None or item is None:
             pass
         self.cache_data[key] = item
 
     def get(self, key):
-        """ Get an item by key
+        """ Return the value from the self.cache_data dict
+        given the key of the item
         """
         if key is None:
             return None
