5cf2de51c829321f2930a8f4fdc4d3cd6f88d144
Epicsociety <measkswho@gmail.com>
2023-11-01 00:33:24 +0300

    determine the best match with our supported languages

---
+++ 0x02-i18n/2-app.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+""" instantiated the Babel object and configure it using the Config
+"""
+from flask import Flask, render_template, request
+from flask_babel import Babel
+
+app = Flask(__name__)
+babel = Babel(app)
+
+
+class Config(object):
+    """ tantiated the Babel object and configure it
+    """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app.config.from_object(Config)
+
+
+@app.route('/')
+def index():
+    """ Render a simple page"""
+    return render_template('1-index.html')
+
+
+@babel.localeselector
+def get_locale():
+    """ determine the best match with our supported languages"""
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
