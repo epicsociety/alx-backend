e803ceee00525dda0d64c2486541e7ea9cec5933
Epicsociety <measkswho@gmail.com>
2023-11-01 00:25:35 +0300

    A basic flask app

--- 0x02-i18n/config.py
+++ 0x02-i18n/config.py
@@ -1,4 +1,6 @@
-class Config:
+class Config(object):
+    """ tantiated the Babel object and configure it
+    """
     LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = "en"
     BABEL_DEFAULT_TIMEZONE = "UTC"
