447492ca92ded4764b52d42ed002897ad97ac713
Epicsociety <measkswho@gmail.com>
2023-11-01 00:21:43 +0300

    A basic flask app

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -1,1 +1,21 @@
 #!/usr/bin/env python3
+""" instantiated the Babel object and configure it using the Config
+"""
+from flask import Flask, render_template
+from flask_babel import Babel
+from config import Config
+
+app = Flask(__name__)
+app.config.from_object(Config)
+
+babel = Babel(app)
+
+
+@app.route('/')
+def index():
+    """ Render a simple page"""
+    return render_template('1-index.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
